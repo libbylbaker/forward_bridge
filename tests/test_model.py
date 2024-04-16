bcb39722efc0cd331394456e8c529b35b9d0e167
Libby Baker <elba@di.ku.dk>
2024-04-16 14:55:59 +0200

    Added new model for different y values

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,9 @@
+import flax.linen as nn
 import jax
 import jax.numpy as jnp
-import flax.linen as nn
 import pytest
 
-from src.models import ScoreMLP, ScoreUNet
+from src.models import ScoreMLP, ScoreMLPDistributedEndpt, ScoreUNet
 
 
 @pytest.fixture
@@ -18,17 +18,20 @@ def t_data():
 
 @pytest.fixture
 def model_setup(output_dim=2):
-    setup = {"output_dim": output_dim,
-             "time_embedding_dim": 32,
-             "init_embedding_dim": 32,
-             "activation": nn.elu,
-             "encoder_layer_dims": [16, 8, 4],
-             "decoder_layer_dims": [4, 8, 16],}
+    setup = {
+        "output_dim": output_dim,
+        "time_embedding_dim": 32,
+        "init_embedding_dim": 32,
+        "activation": nn.elu,
+        "encoder_layer_dims": [16, 8, 4],
+        "decoder_layer_dims": [4, 8, 16],
+    }
     return setup
 
 
-def test_ScoreMLP(x_data, t_data, model_setup):
-    net = ScoreMLP(**model_setup)
+@pytest.mark.parametrize("net_type", [ScoreMLP, ScoreUNet])
+def test_ScoreMLP(x_data, t_data, model_setup, net_type):
+    net = net_type(**model_setup)
     key = jax.random.PRNGKey(0)
     variables = net.init(key, x=x_data, t=t_data, train=False)
     params = variables["params"]
@@ -43,17 +46,17 @@ def test_ScoreMLP(x_data, t_data, model_setup):
     assert score.ndim == 2
 
 
-def test_ScoreUNet(x_data, t_data, model_setup):
-    net = ScoreUNet(**model_setup)
+def testScoreMLPDistributedEndpt(x_data, t_data, model_setup):
+    net = ScoreMLPDistributedEndpt(**model_setup)
     key = jax.random.PRNGKey(0)
-    variables = net.init(key, x=x_data, t=t_data, train=False)
-    params, batch_stats = variables["params"], variables["batch_stats"]
-    score, updates = net.apply(
-        {"params": params, "batch_stats": batch_stats},
+    variables = net.init(key, x=x_data, y=x_data, t=t_data, train=False)
+    params = variables["params"]
+    score = net.apply(
+        {"params": params},
         x=x_data,
+        y=x_data,
         t=t_data,
         train=True,
-        mutable=["batch_stats"]
     )
     assert score.shape[0] == x_data.shape[0]
     assert score.shape[1] == model_setup["output_dim"]
