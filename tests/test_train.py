828e35e5a3cada9ed7c9fdc985e799f8e7ad4c7a
Libby Baker <elba@di.ku.dk>
2024-04-16 15:15:53 +0200

    Fixed test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,10 +1,10 @@
-import jax.random
-import pytest
-
-from src.training.utils import create_train_state, trained_score
 import flax.linen as nn
 import jax.numpy as jnp
+import jax.random
+import pytest
 from flax.training.train_state import TrainState
+
+from src.training.utils import create_train_state, trained_score
 
 
 class Model(nn.Module):
@@ -13,7 +13,6 @@ class Model(nn.Module):
     @nn.compact
     def __call__(self, x, t, train):
         x = nn.Dense(self.output_dim)(x)
-        x = nn.BatchNorm(use_running_average=not train)(x)
         return x
 
 
@@ -33,12 +32,24 @@ def t_data():
 
 
 def test_create_train_state(model, x_data, t_data):
-    state = create_train_state(net=model, key=jax.random.PRNGKey(0), learning_rate=0.1, x_shape=x_data.shape, t_shape=t_data.shape)
+    state = create_train_state(
+        net=model,
+        key=jax.random.PRNGKey(0),
+        learning_rate=0.1,
+        x_shape=x_data.shape,
+        t_shape=t_data.shape,
+    )
     assert isinstance(state.params, dict)
 
 
 def test_trained_score(model, x_data, t_data):
-    state = create_train_state(net=model, key=jax.random.PRNGKey(0), learning_rate=0.1, x_shape=x_data.shape, t_shape=t_data.shape)
+    state = create_train_state(
+        net=model,
+        key=jax.random.PRNGKey(0),
+        learning_rate=0.1,
+        x_shape=x_data.shape,
+        t_shape=t_data.shape,
+    )
     score_fn = trained_score(state)
     score = score_fn(t_data[0], x_data[0])
     assert score.ndim == 1
