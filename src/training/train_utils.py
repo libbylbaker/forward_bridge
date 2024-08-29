c8f5cf4bbe636fcdf9bc9202d09160b50bc87a7b
libbylbaker <elba@di.ku.dk>
2024-08-29 15:13:18 +0200

    Changes to loss (working)

--- src/training/train_utils.py
+++ src/training/train_utils.py
@@ -85,14 +85,22 @@ def _create_train_step(key, model, optimiser, *model_init_sizes, dt, score, data
 
     @jax.jit
     def train_step(params, batch_stats, opt_state, times, trajectory, correction):
-        t, traj, correction, true_score = data_setup(times, trajectory, correction, score)
+        t, traj, correction, true_score, diffusion = data_setup(times, trajectory, correction, score)
 
         def loss_fn(params_):
             prediction, updates = model.apply(
                 {"params": params_, "batch_stats": batch_stats}, traj, t, train=True, mutable=["batch_stats"]
             )
-            loss = 0.5 * dt * jnp.mean(jnp.square(true_score - prediction) * correction)
+            # difference = true_score-prediction
+            # weighted_norm = (difference[:, None, :]@covs)@difference[:, :, None]
+            # loss = 0.5 * dt * jnp.mean(weighted_norm * correction)
+            # return loss, updates
+            sqrt_norm = prediction[:, None, :]@diffusion
+            weighted_norm = sqrt_norm@jnp.moveaxis(sqrt_norm, -1, -2)
+            diff_term = -2*prediction[:, None, :]@true_score[:, :, None]
+            loss = jnp.mean((weighted_norm + diff_term)*correction)
             return loss, updates
+
 
         grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
         (_loss, updates), grads = grad_fn(params)
@@ -113,7 +121,7 @@ def _data_setup_variable_y(times, trajectory, correction, y, score):
 
     time_step_score = jax.vmap(score, in_axes=(0, 0, 0, 0))
     batched_score = jax.vmap(time_step_score, in_axes=(0, 0, 0, 0))
-    true_score = batched_score(t, reversed_traj, t_plus1, reversed_traj_plus1)
+    true_score, covs = batched_score(t, reversed_traj, t_plus1, reversed_traj_plus1)
 
     correction = jnp.repeat(correction, reversed_traj.shape[1] * reversed_traj.shape[2])
     correction = jnp.reshape(correction, (-1, reversed_traj.shape[-1]))
@@ -125,7 +133,7 @@ def _data_setup_variable_y(times, trajectory, correction, y, score):
     t = t.reshape(-1, t.shape[-1])
     true_score = true_score.reshape(-1, reversed_traj.shape[-1])
 
-    return t, reversed_traj, correction, y, true_score
+    return t, reversed_traj, correction, y, true_score, covs
 
 
 @partial(jax.jit, static_argnames=["score"])
@@ -137,7 +145,7 @@ def _data_setup(times, trajectory, correction, score):
 
     time_step_score = jax.vmap(score, in_axes=(0, 0, 0, 0))
     batched_score = jax.vmap(time_step_score, in_axes=(0, 0, 0, 0))
-    true_score = batched_score(t, reversed_traj, t_plus1, reversed_traj_plus1)
+    true_score, covs = batched_score(t, reversed_traj, t_plus1, reversed_traj_plus1)
 
     correction = jnp.repeat(correction, reversed_traj.shape[1] * reversed_traj.shape[2])
     correction = jnp.reshape(correction, (-1, reversed_traj.shape[-1]))
@@ -145,8 +153,9 @@ def _data_setup(times, trajectory, correction, score):
     reversed_traj = reversed_traj.reshape(-1, reversed_traj.shape[-1])
     t = t.reshape(-1, t.shape[-1])
     true_score = true_score.reshape(-1, reversed_traj.shape[-1])
+    covs = covs.reshape(t.shape[0], reversed_traj.shape[-1], -1)
 
-    return t, reversed_traj, correction, true_score
+    return t, reversed_traj, correction, true_score, covs
 
 
 @partial(jax.jit, static_argnames=["score"])
@@ -158,13 +167,15 @@ def _data_setup_forward(times, trajectory, correction, score):
 
     time_step_score = jax.vmap(score, in_axes=(0, 0, 0, 0))
     batched_score = jax.vmap(time_step_score, in_axes=(0, 0, 0, 0))
-    true_score = -batched_score(t, traj, t_plus1, traj_plus1)
+    true_score_minus, covs = batched_score(t, traj, t_plus1, traj_plus1)
+    true_score = -true_score_minus
 
     traj_plus1 = traj_plus1.reshape(-1, traj.shape[-1])
     t_plus1 = t_plus1.reshape(-1, t.shape[-1])
     true_score = true_score.reshape(-1, traj.shape[-1])
+    covs = covs.reshape(-1, traj.shape[-1])
 
-    return t_plus1, traj_plus1, 1.0, true_score
+    return t_plus1, traj_plus1, 1.0, true_score, covs
 
 
 def get_score(drift, diffusion) -> Callable:
@@ -173,10 +184,13 @@ def get_score(drift, diffusion) -> Callable:
         dt = t1 - t0
         drift_last = drift(t0, X0)
         diffusion_last = diffusion(t0, X0)
-        # inv_cov = invert(diffusion_last, diffusion_last.T)
-        # _score = 1 / dt * inv_cov @ (X1 - X0 - dt * drift_last)
-        _score = 1 / dt * (X1 - X0 - dt * drift_last)
-        return _score
+
+        # cov = diffusion_last @ diffusion_last.T
+        diffusion_step_last = (X1 - X0 - dt * drift_last)
+        # _score = 1/dt * jnp.linalg.solve(cov, diffusion_step_last)
+
+        # return _score, cov
+        return 1/dt * diffusion_step_last, diffusion_last
 
     return score
 
