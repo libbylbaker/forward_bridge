5592623d472222fc1c24cdec041d98bbfbbe1cfd
libbylbaker <elba@di.ku.dk>
2024-09-06 14:10:00 +0200

    Added new shape experiment

---
+++ experiments/train_scripts/kunita/fixed_endpt_emoji.py
@@ -0,0 +1,76 @@
+import os.path
+
+import jax.numpy as jnp
+import jax.random as jr
+import optax
+
+from src.models.score_unet import ScoreUNet
+from src.sdes import sde_kunita
+from src.training import train_loop, train_utils
+from src import data_boundary_pts
+
+seed = 1
+
+
+def main(key, T=1.0):
+    num_eye = 5
+    num_brow = 5
+    num_mouth = 5
+    num_outline = 25
+
+    num_landmarks = num_mouth + num_outline + num_eye + num_brow
+
+    fns = data_boundary_pts.smiley_face_fns(num_eye, num_brow, num_mouth, num_outline)
+    pts = tuple(f() for f in fns)
+    y = jnp.concatenate(pts, axis=-1).T.flatten()
+    y = y/jnp.max(jnp.abs(y))
+
+    sde = {"N": 100, "dim": y.size, "T": T, "y": y}
+    dt = sde["T"] / sde["N"]
+
+    checkpoint_path = os.path.abspath(f"../../checkpoints/kunita/emoji_smiley")
+
+    network = {
+        "output_dim": sde["dim"],
+        "time_embedding_dim": 64,
+        "init_embedding_dim": 64,
+        "activation": "leaky_relu",
+        "encoder_layer_dims": [8*num_landmarks, 4*num_landmarks, 2*num_landmarks, num_landmarks],
+        "decoder_layer_dims": [num_landmarks, 2*num_landmarks, 4*num_landmarks, 8*num_landmarks],
+        "batch_norm": True,
+    }
+
+    training = {
+        "batch_size": 100,
+        "load_size": 5000,
+        "num_reloads": 1000,
+        "lr": 5e-3,
+        "epochs_per_load": 1,
+    }
+
+    drift, diffusion = sde_kunita.vector_fields()
+    data_fn = sde_kunita.data_reverse(sde["y"], sde["T"], sde["N"])
+
+    model = ScoreUNet(**network)
+    optimiser = optax.chain(optax.adam(learning_rate=training["lr"]))
+
+    score_fn = train_utils.get_score(drift=drift, diffusion=diffusion)
+
+    x_shape = jnp.empty(shape=(1, sde["dim"]))
+    t_shape = jnp.empty(shape=(1, 1))
+    model_init_sizes = (x_shape, t_shape)
+
+    (loop_key, train_key) = jr.split(key, 2)
+
+    train_step, params, opt_state, batch_stats = train_utils.create_train_step_reverse(
+        train_key, model, optimiser, *model_init_sizes, dt=dt, score=score_fn
+    )
+
+    train_loop.train(
+        loop_key, training, data_fn, train_step, params, batch_stats, opt_state, sde, network, checkpoint_path
+    )
+
+
+if __name__ == "__main__":
+    main_key = jr.PRNGKey(seed)
+    main(main_key)
--- src/data_boundary_pts.py
+++ src/data_boundary_pts.py
@@ -34,29 +34,38 @@ def plot_functions(functions):
 def smiley_face_fns(num_eye, num_brow, num_mouth, num_outline):
     def left_eye():
         x = np.linspace(-2.4, -0.6, num_eye)
-        return x, -((x + 1.4) ** 2 / 5 - 0.2)
+        y = -((x + 1.4) ** 2 / 5 - 0.2)
+        return jnp.stack([x, y], axis=1).flatten()
+
 
     def right_eye():
         x = np.linspace(0.6, 2.4, num_eye)
-        return x, -((x - 1.4) ** 2 / 5 - 0.2)
+        y = -((x - 1.4) ** 2 / 5 - 0.2)
+        return jnp.stack([x, y], axis=1).flatten()
+
 
     def left_brow():
         x = np.linspace(-3, -1, num_brow)
-        return x, -(((x + 1.5) / 2.6) ** 2) + 1.8
+        y = -(((x + 1.5) / 2.6) ** 2) + 1.8
+        return jnp.stack([x, y], axis=1).flatten()
+
 
     def right_brow():
         x = np.linspace(1, 3, num_brow)
-        return x, -(((x - 1.5) / 2.6) ** 2) + 1.8
+        y = -(((x - 1.5) / 2.6) ** 2) + 1.8
+        return jnp.stack([x, y], axis=1).flatten()
+
 
     def mouth():
         x = np.linspace(-1.3, 1.3, num_mouth)
-        return x, (x**2) / 2.5 - 2.7
+        y = (x**2) / 2.5 - 2.7
+        return jnp.stack([x, y], axis=1).flatten()
 
     def outline():
         theta = np.linspace(0, 2 * np.pi, num_outline, endpoint=True)
         x = np.cos(theta)
         y = np.sin(theta)
         r = 4.5
-        return r * x, r * y
+        return jnp.stack([r * x, r * y], axis=1).flatten()
 
     return [left_eye, right_eye, left_brow, right_brow, mouth, outline]
