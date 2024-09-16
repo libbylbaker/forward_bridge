c959d343dc50dbe6456b204f69b814af09cea508
libbylbaker <elba@di.ku.dk>
2024-09-16 11:11:37 +0200

    Updated test_score. Passes again

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,6 +1,6 @@
 import jax.numpy as jnp
 
-from src.sdes import sde_bm as bm
+from src.sdes import sde_bm
 from src.sdes import sde_ornstein_uhlenbeck
 from src.training.train_utils import get_score
 
@@ -10,7 +10,7 @@ def test_score():
     t1 = 0.5
     Y0 = jnp.array([0.0])
     Y1 = jnp.array([1.0])
-    drift, diffusion = bm.vector_fields()
-    score_fn = get_score(drift, diffusion)
-    _score = score_fn(t0, Y0, t1, Y1)
+    bm = sde_bm.brownian_motion(T=1., N=10, dim=1)
+    score_fn = get_score(bm)
+    _score, cov = score_fn(t0, Y0, t1, Y1)
     assert _score == jnp.array([2.0])
