753376ac0920c2d892ea65b5185fe62ab579806d
Libby Baker <elba@di.ku.dk>
2024-09-18 09:41:37 +0200

    Added other butterfly

--- src/data_boundary_pts.py
+++ src/data_boundary_pts.py
@@ -160,6 +160,12 @@ def butterfly_tom(num_landmarks=100):
     return butterfly("../../data/inverted_butterfly_tom.png", -2, num_landmarks)
 
 
+def butterfly1(num_landmarks=100):
+    path = "../../data/inverted_butterfly1.png"
+    remove_pts = -466
+    return butterfly(path, remove_pts, num_landmarks)
+
+
 def butterfly2(num_landmarks=100):
     path = "../../data/inverted_butterfly2.png"
     remove_pts = -70
