779e90bc15a3f63c7b002193002501c7bbb1cef8
libbylbaker <elba@di.ku.dk>
2024-09-19 17:10:36 +0200

    Added emoji experiment (works)

--- experiments/train_scripts/kunita/fw/train/emoji_neuralop.py
+++ experiments/train_scripts/kunita/fw/train/emoji_neuralop.py
@@ -6,63 +6,61 @@ import optax
 import orbax.checkpoint
 
 from src import data_boundary_pts
-from src.models.score_unet import ScoreUNet
+from src.models.neuralop import CTUNO1D
 from src.sdes import sde_kunita, sde_data
 from src.training import train_loop, train_utils
 
 seed = 1
 
-
 def main(key):
 
     load = False
 
-    num_eye = 5
-    num_brow = 5
-    num_mouth = 5
-    num_outline = 20
+    num_eye = 4
+    num_brow = 4
+    num_mouth = 4
+    num_outline = 10
 
     num_landmarks = num_mouth + num_outline + 2*num_eye + 2*num_brow
 
     fns = data_boundary_pts.smiley_face_fns(num_eye, num_brow, num_mouth, num_outline)
     x0 = data_boundary_pts.flattened_array_from_faces(fns)
 
-    sigma = 0.2
-    kappa = 1 / (sigma * jnp.sqrt(2 * jnp.pi))
+    sigma = 0.04
+    kappa = 1 / sigma
 
-    sde_args = {"T": 2., "N": 100, "num_landmarks": num_landmarks, "sigma": sigma, "kappa": kappa, "grid_size": 25}
+    sde_args = {"T": 1., "N": 100, "x0": x0, "num_landmarks": num_landmarks, "sigma": sigma, "kappa": kappa, "grid_size": 50, "grid_range": (-1.25, 1.25)}
     kunita = sde_kunita.kunita(**sde_args)
 
     dt = kunita.T/kunita.N
 
-    checkpoint_path = os.path.abspath(f"../../checkpoints/kunita/fw/emoji_{kunita.T}_{sigma}")
+    checkpoint_path = os.path.abspath(f"../../../../../checkpoints/kunita/fw/emoji_neuralop")
 
     network = {
-        "output_dim": kunita.dim,
-        "time_embedding_dim": 64,
-        "init_embedding_dim": 64,
-        "activation": "leaky_relu",
-        "encoder_layer_dims": [8*num_landmarks, 4*num_landmarks, 2*num_landmarks, num_landmarks],
-        "decoder_layer_dims": [num_landmarks, 2*num_landmarks, 4*num_landmarks, 8*num_landmarks],
-        "batch_norm": True,
+        "out_co_dim": 2,
+        "lifting_dim": 16,
+        "act": "gelu",
+        "co_dims_fmults": [1, 2, 4, 4],
+        "n_modes_per_layer": [8, 6, 4, 4],
+        "norm": "batch"
     }
 
     training = {
         "batch_size": 128,
         "epochs_per_load": 1,
         "lr": 5e-3,
-        "num_reloads": 6000,
+        "num_reloads": 100,
         "load_size": 2048,
     }
 
-    data_gen = sde_data.data_forward(x0, kunita)
-    model = ScoreUNet(**network)
+    data_gen = sde_data.data_forward(jnp.zeros_like(x0), kunita)
+    model = CTUNO1D(**network)
     optimiser = optax.chain(optax.adam(learning_rate=training["lr"]))
 
     score_fn = train_utils.get_score(kunita)
 
-    x_shape = jnp.empty(shape=(1, num_landmarks, 2))
-    t_shape = jnp.empty(shape=(1, 1))
+    x_shape = jnp.empty(shape=(10, num_landmarks, 2))
+    t_shape = jnp.empty(shape=(10, 1))
     model_init_sizes = (x_shape, t_shape)
 
     (loop_key, train_key) = jr.split(key, 2)
